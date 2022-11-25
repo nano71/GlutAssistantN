edf31c25dca19c8f7c078bcd4243f550f8e65c46
nano71 <80870408+nano71@users.noreply.github.com>
2022-11-25 23:54:14 +0800

    例行提交

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-flutter build apk --obfuscate --split-debug-info=HLQ_Struggle --target-platform android-arm,android-arm64,android-x64 --split-per-abi --no-sound-null-safety
+flutter build apk --flavor build --obfuscate --split-debug-info=HLQ_Struggle --target-platform android-arm64 --split-per-abi --no-sound-null-safety
