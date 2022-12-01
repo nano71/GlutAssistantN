12e4ba3f03ec5f581ac057ebcc0160288eb0d8b1
nano71 <80870408+nano71@users.noreply.github.com>
2022-12-01 19:56:46 +0800

    例行提交

--- lib/pages/init.dart
+++ lib/pages/init.dart
@@ -16,28 +16,12 @@ class CustomRoute extends PageRouteBuilder {
 
   CustomRoute(this.widget, [int milliseconds = 300])
       : super(
-            //父类的方法
-            //设置动画持续的时间，建议再1和2之间
             transitionDuration: Duration(milliseconds: milliseconds),
-            //页面的构造器
-            pageBuilder: (
-              BuildContext context,
-              Animation<double> animation,
-              //次级动画
-              Animation<double> secondaryAnimation,
-            ) {
+            pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
               return widget;
             },
-            //过度效果
             transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
-              // 过度的动画的值
-              return FadeTransition(
-                // 过度的透明的效果
-                opacity: Tween(begin: 0.0, end: 1.0)
-                    // 给他个透明度的动画   CurvedAnimation：设置动画曲线
-                    .animate(CurvedAnimation(parent: animation, curve: Curves.ease)),
-                child: child,
-              );
+              return FadeTransition(opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: animation, curve: Curves.ease)), child: child);
             });
 }
 
