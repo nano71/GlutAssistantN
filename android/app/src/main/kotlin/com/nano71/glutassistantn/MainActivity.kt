14e24c20b2e4f12a21ec8dac664a7b6b7d930dcc
nano71 <80870408+nano71@users.noreply.github.com>
2023-09-21 01:10:41 +0800

    完善appwidget

--- android/app/src/main/kotlin/com/nano71/glutassistantn/MainActivity.kt
+++ android/app/src/main/kotlin/com/nano71/glutassistantn/MainActivity.kt
@@ -1,25 +1,6 @@
 package com.nano71.glutassistantn
 
-import android.content.Intent
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugin.common.MethodChannel
 
-
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL_NAME)
-            .setMethodCallHandler { call, result ->
-                if (call.method == "startMainApp") {
-                    // 启动主程序
-                    val intent = Intent(applicationContext, YourMainActivity::class.java)
-                    startActivity(intent)
-                }
-            }
-    }
-
-    companion object {
-        private const val CHANNEL_NAME = "your_channel_name"
-    }
+class MainActivity: FlutterActivity() {
 }
