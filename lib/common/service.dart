f79de4738d470423ce258de31b7c3b0aa04de337
nano71 <80870408+nano71@users.noreply.github.com>
2024-10-10 11:11:17 +0800

    优化组件调用

--- lib/common/service.dart
+++ lib/common/service.dart
@@ -1,19 +1,18 @@
 import 'package:workmanager/workmanager.dart';
-
 import 'homeWidget.dart';
 
-@pragma('vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+@pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
     print("执行任务: $task");
-    // 在这里处理你的后台任务逻辑
+
     try {
       await backstageRefresh();
-    } catch (err) {
-      print('callbackDispatcher: ' + err.toString());
-      throw Exception(err);
+    } catch (error) {
+      print('callbackDispatcher: ' + error.toString());
+      throw Exception(error);
     }
-    // 返回 true 代表任务成功，false 代表任务失败
+
     return Future.value(true);
   });
 }
