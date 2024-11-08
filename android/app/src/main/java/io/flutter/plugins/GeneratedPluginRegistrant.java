9e934f5d9fd4573189e9823755db13bd696e63cf
nano71 <80870408+nano71@users.noreply.github.com>
2024-11-08 17:17:30 +0800

    日志存储

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -31,6 +31,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin path_provider_android, io.flutter.plugins.pathprovider.PathProviderPlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new io.sentry.flutter.SentryFlutterPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin sentry_flutter, io.sentry.flutter.SentryFlutterPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.urllauncher.UrlLauncherPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin url_launcher_android, io.flutter.plugins.urllauncher.UrlLauncherPlugin", e);
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,33 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:glutassistantn/common/log.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:workmanager/workmanager.dart';
 
 import '/pages/Layout.dart';
 import 'common/service.dart';
 import 'config.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent, systemNavigationBarColor: Colors.white));
-  print("startApp...");
-  runApp(App());
+void main() async {
+  runZoned(() {
+    SentryFlutter.init((options) {
+      options.tracesSampleRate = 1.0;
+      options.profilesSampleRate = 1.0;
+      options.experimental.replay.sessionSampleRate = 1.0;
+      options.experimental.replay.onErrorSampleRate = 1.0;
+    }, appRunner: () {
+      WidgetsFlutterBinding.ensureInitialized();
+      Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent, systemNavigationBarColor: Colors.white));
+      print("startApp...");
+      runApp(App());
+    });
+  }, zoneSpecification: new ZoneSpecification(print: (Zone self, ZoneDelegate parent, Zone zone, String line) {
+    parent.print(zone, line);
+  }));
 }
 
 class App extends StatelessWidget {
@@ -28,4 +44,3 @@ class App extends StatelessWidget {
         home: DataPreloadPage());
   }
 }
-
