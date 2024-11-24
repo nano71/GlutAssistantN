b1c3abee51cfb8083f45bc92cb6986d59d85662d
nano71 <80870408+nano71@users.noreply.github.com>
2024-11-25 00:15:50 +0800

    修复特殊课表的解析错误2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:glutassistantn/common/log.dart';
